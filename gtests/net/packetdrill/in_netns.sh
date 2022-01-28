2713effff04c1cc787c54ff35dfe9c52a0c99add
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-01-28 14:31:16 +0100

    packetdrill: run_all: arg to capture packet per test
    This is useful for debug purposes.
    
    With the new option, we can specify to capture packets with TCPDump and
    write the .pcap files in the given directory.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 48a0a2a8a213e958bf0511e75999317112dd66a9)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- gtests/net/packetdrill/in_netns.sh
+++ gtests/net/packetdrill/in_netns.sh
@@ -6,12 +6,23 @@ set -e
 
 readonly NETNS="ns-$(mktemp -u XXXXXX)"
 
+TCPDUMP_PID=
+
 setup() {
 	ip netns add "${NETNS}"
 	ip -netns "${NETNS}" link set lo up
+	if [ -n "${TCPDUMP_OUTPUT}" ]; then
+		ip netns exec "${NETNS}" tcpdump -i any -s 150 -w "${TCPDUMP_OUTPUT}" &
+		TCPDUMP_PID=$!
+		sleep 1
+	fi
 }
 
 cleanup() {
+	if [ -n "${TCPDUMP_PID}" ]; then
+		kill "${TCPDUMP_PID}"
+		wait "${TCPDUMP_PID}" 2>/dev/null || true
+	fi
 	ip netns del "${NETNS}"
 }
 
