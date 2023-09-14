989feda799f2cadd0c15dee323798cf7c2729eae
Matthieu Baerts <matthieu.baerts@tessares.net>
2023-09-14 15:51:39 +0200

    packetdrill: run_all: support timeout with --serialized
    Before, when using the --serialized argument, the script was simply
    waiting for the packetdrill command to finish but without any timeout.
    
    Now that the limitation of the parallel tasks has been put in place, it
    is possible to easily support this timeout feature.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 9f79485415fea0180da07fe8c86f0189c07d6190)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- gtests/net/packetdrill/run_all.py
+++ gtests/net/packetdrill/run_all.py
@@ -119,8 +119,6 @@ class TestSet(object):
 
     time_start = time.time()
     process = subprocess.Popen(cmd, stdout=outfile, stderr=errfile, cwd=execdir)
-    if self.args['serialized']:
-      process.wait()
 
     return (process, path, variant, outfile, errfile, time_start)
 
@@ -146,7 +144,7 @@ class TestSet(object):
 
   def StartPollTestSet(self, cmds):
     """Start and wait until all tests in procs have finished or until timeout."""
-    max_in_parallel = self.args['max_in_parallel']
+    max_in_parallel = 1 if self.args['serialized'] else self.args['max_in_parallel']
     if max_in_parallel == 0 or max_in_parallel > len(cmds):
       max_in_parallel = len(cmds)
 
